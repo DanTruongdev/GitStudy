09dec5ded2d5ef7e135ac4923cedc7bf7ed4f5de
Dantruongdev <dantruong2002tq@gmail.com>
2024-07-04 10:35:00 +0700

    the commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ConsoleApp1;
+public interface IEngine
+{
+    void Start();
+}
+
+public class Engine : IEngine
+{
+    public void Start()
+    {
+        Console.WriteLine("Engine started.");
+    }
+}
+
+// public class Car
+// {
+//     private readonly IEngine _engine;
+
+//     public Car(IEngine engine) // Phụ thuộc được truyền vào thông qua constructor
+//     {
+//         _engine = engine;
+//     }
+
+//     public void Drive()
+//     {
+//         _engine.Start();
+//         Console.WriteLine("Car is driving.");
+//     }
+// }
+public class Car
+{
+    public IEngine? Engine {get; set; }
+
+    public void Drive()
+    {
+        Engine.Start();
+        Console.WriteLine("Car is driving.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      IEngine engine = new Engine();
+      Car car = new Car();
+      car.Engine = engine;
+      car.Drive();
+    }
+}
